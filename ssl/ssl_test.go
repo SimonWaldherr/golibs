f28c4ab437aa622baf0be4209960b419009bb850
Simon Waldherr <git@simon.waldherr.eu>
2015-07-10 03:39:53 +0200

    better test coverage of ssl lib no more go 1.1 and go 1.2 builds COVERAGE TEST ansi coverage: 100.0% of statements arg coverage: 89.5% of statements as coverage: 98.0% of statements cache coverage: 97.1% of statements file coverage: 85.3% of statements graphics coverage: 96.6% of statements re coverage: 100.0% of statements regex coverage: 87.5% of statements ssl coverage: 90.4% of statements stack coverage: 100.0% of statements xmath coverage: 100.0% of statements

--- ssl/ssl_test.go
+++ ssl/ssl_test.go
@@ -6,9 +6,12 @@ import (
 
 func Test_Main(t *testing.T) {
 	err := Check("ssl_gwv.cert", "ssl_gwv.key")
+	options := map[string]string{}
+	Generate(options)
+
 	if err != nil {
 		t.Log("Couldn't find certs, trying to create new ones\n")
-		options := map[string]string{}
+
 		options["certPath"] = "x.cert"
 		options["keyPath"] = "x.key"
 		options["host"] = "*"
@@ -21,5 +24,21 @@ func Test_Main(t *testing.T) {
 		if err != nil {
 			t.Fatalf("Couldn't create certs: %v\n", err)
 		}
+	} else {
+		t.Fatalf("There should be an error about a missing file\n")
+	}
+	err = Check("x.cert", "ssl_gwv.key")
+	if err == nil {
+		t.Fatalf("There should be an error about a missing file\n")
+	}
+	options["keyPath"] = "/not/existing/path/x.key"
+	err = Generate(options)
+	if err == nil {
+		t.Fatalf("There should be an error about a not writeable path\n", err)
+	}
+	options["certPath"] = "/not/existing/path/x.cert"
+	err = Generate(options)
+	if err == nil {
+		t.Fatalf("There should be an error about a not writeable path\n", err)
 	}
 }
