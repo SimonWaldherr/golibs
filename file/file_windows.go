a8fc8cca72a3576b890e0d830793bc29c4b7309f
Simon Waldherr <git@simon.waldherr.eu>
2015-10-12 01:08:11 +0200

    fix file.Time on windows (hopefully)

--- file/file_windows.go
+++ file/file_windows.go
@@ -12,7 +12,6 @@ func Time(fn string) (time.Time, time.Time, time.Time, error) {
 		return time.Time{}, time.Time{}, time.Time{}, err
 	}
 	mtime := file.ModTime()
-	stat := file.Sys().(*syscall.Stat_t)
 	atime := time.Unix(0, file.Sys().(*syscall.Win32FileAttributeData).LastAccessTime.Nanoseconds())
 	ctime := time.Unix(0, file.Sys().(*syscall.Win32FileAttributeData).LastWriteTime.Nanoseconds())
 	return atime, mtime, ctime, nil
