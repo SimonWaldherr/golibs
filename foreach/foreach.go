4e0e28fc86c31673b822bd17c056b71e35842e15
Simon Waldherr <git@simon.waldherr.eu>
2015-10-18 20:51:29 +0200

    put Node/Object functions to separate package

--- foreach/foreach.go
+++ foreach/foreach.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"os"
 	"simonwaldherr.de/go/golibs/file"
+	"simonwaldherr.de/go/golibs/node"
 	//"encoding/xml"
 )
 
@@ -16,7 +17,7 @@ func JSON(str string, handler func(*string, *int, *interface{}, int)) error {
 	var j interface{}
 	err := json.Unmarshal([]byte(str), &j)
 	if err == nil {
-		Node(&j, handler)
+		node.Node(&j, handler)
 		return nil
 	}
 	return err
@@ -32,27 +33,3 @@ func XML(str string, handler func(*string, *int, *interface{}, int)) error {
 	}
 	return err
 }*/
-
-func Node(in *interface{}, handler func(*string, *int, *interface{}, int)) {
-	nodeHelper(in, handler, 0)
-}
-
-func nodeHelper(node *interface{}, handler func(*string, *int, *interface{}, int), depth int) {
-	if node == nil {
-		return
-	}
-	o, isObject := (*node).(map[string]interface{})
-	if isObject {
-		for k, v := range o {
-			handler(&k, nil, &v, depth)
-			nodeHelper(&v, handler, depth+1)
-		}
-	}
-	a, isArray := (*node).([]interface{})
-	if isArray {
-		for i, x := range a {
-			handler(nil, &i, &x, depth)
-			nodeHelper(&x, handler, depth+1)
-		}
-	}
-}
---
+++ node/node.go
@@ -0,0 +1,25 @@
+package node
+
+func Node(in *interface{}, handler func(*string, *int, *interface{}, int)) {
+  nodeHelper(in, handler, 0)
+}
+
+func nodeHelper(node *interface{}, handler func(*string, *int, *interface{}, int), depth int) {
+  if node == nil {
+    return
+  }
+  o, isObject := (*node).(map[string]interface{})
+  if isObject {
+    for k, v := range o {
+      handler(&k, nil, &v, depth)
+      nodeHelper(&v, handler, depth+1)
+    }
+  }
+  a, isArray := (*node).([]interface{})
+  if isArray {
+    for i, x := range a {
+      handler(nil, &i, &x, depth)
+      nodeHelper(&x, handler, depth+1)
+    }
+  }
+}
