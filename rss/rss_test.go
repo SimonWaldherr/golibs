f4e4d44d6d2f54475f222223d1340b13db72b839
Simon Waldherr <git@simon.waldherr.eu>
2015-11-29 22:36:48 +0100

    more test coverage for rss pkg

--- rss/rss_test.go
+++ rss/rss_test.go
@@ -6,8 +6,9 @@ import (
 
 var feeds = []string{
 	"http://cre.fm/feed/m4a",
-	"http://www.konscience.de/feed/aac/",
-	"http://podcast.firtz.org/firtz/mp3"}
+	"http://konscience.de/feed/aac/",
+	"http://podcast.firtz.org/firtz/mp3",
+}
 
 func Test_Read(t *testing.T) {
 	for _, url := range feeds {
@@ -16,15 +17,30 @@ func Test_Read(t *testing.T) {
 			t.Fatalf("rss.Read Test failed: %v\nFeed: %v\n\n", err, url)
 		}
 		if feedContent.Title == "" {
-			t.Fatalf("rss.Read Test failed: No Title detected\nFeed:%v\n\n", url)
+			t.Fatalf("rss.Read Test failed: No Title detected\nFeed: %v\n\n", url)
 		}
 		if feedContent.Len() < 3 {
-			t.Fatalf("rss.Read Test failed: I don't think that there are only %v Episodes\nFeed:%v | %v\n\n", feedContent.Len(), url, feedContent.Title)
+			t.Fatalf("rss.Read Test failed: I don't think that there are only %v Episodes\nFeed: %v | %v\n\n", feedContent.Len(), url, feedContent.Title)
 		}
+
 		for _, item := range feedContent.Items {
 			if item.Time().Unix() < 921456000 {
-				t.Fatalf("rss.Read Test failed: The episode publication date is before the release date of the first RSS draft\nFeed:%v | %v\n\n", url, feedContent.Title)
+				t.Fatalf("rss.Read Test failed: The episode publication date is before the release date of the first RSS draft\nFeed: %v | %v\n\n", url, feedContent.Title)
 			}
 		}
+	}
+}
+
+func Test_Read2(t *testing.T) {
+	_, err := Read("http://localhost/feed.xml")
+	if err == nil {
+		t.Fatal("rss.Read2 Test failed: I expect an error but got nil\n")
+	}
+}
+
+func Test_Main_Time(t *testing.T) {
+	feedContent, _ := Read("http://cre.fm/feed/m4a")
+	if feedContent.Time().Unix() < 921456000 {
+		t.Fatal("rss.Main.Time Test failed: The specified last build date is before the release date of the first RSS draft\n")
 	}
 }
