b8c967eca62e8b523545515db10d8ba55fc9e910
Simon Waldherr <git@simon.waldherr.eu>
2018-10-21 21:30:37 +0200

    fix bug in csv package forgot maps are sorted randomly

--- csv/csv_test.go
+++ csv/csv_test.go
@@ -2,6 +2,7 @@ package csv
 
 import (
 	"testing"
+	"sort"
 )
 
 var userdata string = `id;name;email
@@ -9,16 +10,35 @@ var userdata string = `id;name;email
 1;Jane Doe;jane.doe@example.com
 2;Max Mustermann;m.mustermann@alpha.tld`
 
+type kv struct {
+	Key   int
+	Value []string
+}
+
+func sorting(m map[int][]string) []kv {
+	var ss []kv
+	for k, v := range m {
+		ss = append(ss, kv{k, v})
+	}
+	sort.Slice(ss, func(i, j int) bool {
+		return ss[i].Value[1] > ss[j].Value[1]
+	})
+	return ss
+}
+
 func Test_LoadCSVfromString(t *testing.T) {
 	var str string
-	var should string = "John Doe, Jane Doe, Max Mustermann"
+	var should string = "Max Mustermann, John Doe, Jane Doe"
 
-	csvmap, k := LoadCSVfromString(userdata)
-	for _, user := range csvmap {
+	csvmap, _ := LoadCSVfromString(userdata)
+	
+	sortedmap := sorting(csvmap)
+	
+	for _, user := range sortedmap {
 		if str != "" {
 			str += ", "
 		}
-		str += user[k["name"]]
+		str += string(user.Value[1])
 	}
 	if str != should {
 		t.Fatalf("user is \"%s\", should be \"%s\"\n", str, should)
--- csv/example_test.go
+++ csv/example_test.go
@@ -3,6 +3,7 @@ package csv_test
 import (
 	"fmt"
 	"simonwaldherr.de/go/golibs/csv"
+	"sort"
 )
 
 var userdata string = `id;name;email
@@ -10,24 +11,56 @@ var userdata string = `id;name;email
 1;Jane Doe;jane.doe@example.com
 2;Max Mustermann;m.mustermann@alpha.tld`
 
+var userdata2 string = `id;name;email
+0;John Doe;jDoe@example.org`
+
+type kv struct {
+	Key   int
+	Value []string
+}
+
+func sorting(m map[int][]string) []kv {
+	var ss []kv
+	for k, v := range m {
+		ss = append(ss, kv{k, v})
+	}
+	sort.Slice(ss, func(i, j int) bool {
+		return ss[i].Value[1] > ss[j].Value[1]
+	})
+	return ss
+}
+
 func Example() {
-	csvmap, k := csv.LoadCSVfromString(userdata)
-	for _, user := range csvmap {
-		fmt.Println(user[k["name"]])
+	csvmap, _ := csv.LoadCSVfromString(userdata)
+	sortedmap := sorting(csvmap)
+	
+	for _, user := range sortedmap {
+		fmt.Println(string(user.Value[1]))
 	}
 
-	// Output: John Doe
+	// Output: Max Mustermann
+	// John Doe
 	// Jane Doe
-	// Max Mustermann
 }
 
 func Example_second() {
-	csvmap, k := csv.LoadCSVfromFile("./test.csv")
-	for _, user := range csvmap {
-		fmt.Println(user[k["name"]])
+	csvmap, _ := csv.LoadCSVfromFile("./test.csv")
+	sortedmap := sorting(csvmap)
+	
+	for _, user := range sortedmap {
+		fmt.Println(string(user.Value[1]))
 	}
 
-	// Output: John Doe
+	// Output: Max Mustermann
+	// John Doe
 	// Jane Doe
-	// Max Mustermann
+}
+
+func Example_third() {
+	csvmap, k := csv.LoadCSVfromString(userdata2)
+	for _, user := range csvmap {
+		fmt.Println(user[k["email"]])
+	}
+
+	// Output: jDoe@example.org
 }
